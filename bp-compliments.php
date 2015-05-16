884bba847b194c11a514d6647a5a470091da1ef4
Giri <giridharanbtech@gmail.com>
2015-05-16 20:20:47 +0530

    added field for post_id

--- bp-compliments.php
+++ bp-compliments.php
@@ -17,6 +17,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 function bp_compliments_init() {
     global $wpdb, $bp;
     // some pertinent defines
+    define( 'BP_COMPLIMENTS_VER', 0.1 );
     define( 'BP_COMPLIMENTS_DIR', dirname( __FILE__ ) );
     define( 'BP_COMPLIMENTS_URL', plugin_dir_url( __FILE__ ) );
     if ( !$table_prefix = $bp->table_prefix )
@@ -43,14 +44,17 @@ add_action( 'bp_include', 'bp_compliments_init' );
 
 function bp_compliments_activate() {
     global $bp, $wpdb;
+    $version = get_option( 'bp_compliments_version');
 
-    $charset_collate = !empty( $wpdb->charset ) ? "DEFAULT CHARACTER SET $wpdb->charset" : '';
-    if ( !$table_prefix = $bp->table_prefix )
-        $table_prefix = apply_filters( 'bp_core_get_table_prefix', $wpdb->base_prefix );
+    if (!$version) {
+        $charset_collate = !empty( $wpdb->charset ) ? "DEFAULT CHARACTER SET $wpdb->charset" : '';
+        if ( !$table_prefix = $bp->table_prefix )
+            $table_prefix = apply_filters( 'bp_core_get_table_prefix', $wpdb->base_prefix );
 
-    $sql[] = "CREATE TABLE IF NOT EXISTS {$table_prefix}bp_compliments (
+        $sql[] = "CREATE TABLE IF NOT EXISTS {$table_prefix}bp_compliments (
 			id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 			term_id int(10) NOT NULL,
+			post_id int(10) NULL DEFAULT NULL,
 			receiver_id bigint(20) NOT NULL,
 			sender_id bigint(20) NOT NULL,
 			message varchar(1000) NULL DEFAULT NULL,
@@ -58,8 +62,10 @@ function bp_compliments_activate() {
 		        KEY compliments (receiver_id, sender_id)
 		) {$charset_collate};";
 
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+        update_option( 'bp_compliments_version', BP_COMPLIMENTS_VER );
+    }
 }
 register_activation_hook( __FILE__, 'bp_compliments_activate' );
 
