1990f1e9c6af65c5be57afe4ddad5a2f51947aea
Giri <giridharanbtech@gmail.com>
2015-05-16 17:48:23 +0530

    get user id directly

--- includes/bp-compliments-forms.php
+++ includes/bp-compliments-forms.php
@@ -3,18 +3,14 @@ function handle_compliments_form_data() {
     if (isset($_POST['comp-modal-form'])) {
         $term_id = strip_tags(esc_sql($_POST['term_id']));
         $message = strip_tags(esc_sql($_POST['message']));
-        $sender_id = strip_tags(esc_sql($_POST['sender_id']));
-        $receiver_id = strip_tags(esc_sql($_POST['receiver_id']));
         $args = array(
-            'sender_id' => (int) $sender_id,
-            'receiver_id' => (int) $receiver_id,
             'term_id' => (int) $term_id,
             'message' => $message
         );
         bp_compliments_start_compliment($args);
     }
 }
-add_action( 'bp_loaded', 'handle_compliments_form_data', 99 );
+add_action( 'bp_init', 'handle_compliments_form_data', 99 );
 
 
 function bp_compliments_modal_form() {
@@ -55,8 +51,6 @@ function bp_compliments_modal_form() {
 
                         ?>
                         <textarea name="message" maxchar="100"></textarea>
-                        <input type="hidden" value="<?php echo bp_loggedin_user_id(); ?>" name="sender_id"/>
-                        <input type="hidden" value="<?php echo bp_displayed_user_id(); ?>" name="receiver_id"/>
                         <div class="yepp-pop-buttons">
                             <button type="submit" class="comp-submit-btn" name="comp-modal-form" value="submit">Send</button>
                             <a class="bp-comp-cancel" href="#">Cancel</a>
